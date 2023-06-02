d8e376f4826c4b69532bc0cfebc6ff61a8253f2d
sa <halil@DESKTOP-TSHLOIG>
2023-06-02 22:43:53 +0300

    Add project files.

---
+++ qwwqwqwqwqwq/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qwwqwqwqwqwq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 ödevimiz
+
+            //string[] adlar = { "M,E,V,L,Ü,T" };
+
+
+            //for (int i = 0; i < adlar.Length; i++)
+            //{
+
+            //    Console.WriteLine(adlar[i]);
+            //    adlar[i] = Console.ReadLine();
+
+            //}
+            //Console.WriteLine("İsim sarayınız:");
+            //for (int i = 0; i < adlar.Length; i++)
+            //{
+            //    Console.WriteLine(adlar[i]);
+            //}
+            //for (int i = 0; i < 7; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    i = 0;
+            //    i++;
+            //}
+            //for (int a = 0; a < 10; a--)
+            //{
+            //    Console.WriteLine(a); Console.WriteLine(a + "");
+            //    a--;
+            //}
+
+            //Console.Write("Kaç adet ürün gireceksiniz? ");
+            //int urunSayisi = int.Parse(Console.ReadLine());
+
+            //string[] urunler = new string[urunSayisi];
+            ////decimal[] fiyatlar = new decimal[urunSayisi];
+
+            ////for (int i = 0; i < urunSayisi; i++)
+            ////{
+            ////    Console.Write($"Ürün {i + 1} adı: ");
+            //    urunler[i] = Console.ReadLine();
+
+            //    Console.Write($"Ürün {i + 1} fiyatı: ");
+            //    fiyatlar[i] = decimal.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Ürünler ve Fiyatları:");
+            //for (int i = 0; i < urunSayisi; i++)
+            //{
+            //    Console.WriteLine($"{urunler[i]} - {fiyatlar[i]}");
+            //}
+
+
+
+
+        }
+    }
+}
